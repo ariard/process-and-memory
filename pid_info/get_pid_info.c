9449a9f9d8684923f65aaa2f113f2f4cf6bcf350
Antoine Riard <ariard@student.42.fr>
2018-04-29 16:27:24 +0200

    [core] try copy_to_user

--- pid_info/get_pid_info.c
+++ pid_info/get_pid_info.c
@@ -2,6 +2,13 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/syscalls.h>
+#include <linux/fs.h>
+#include <linux/pid.h>
+#include <linux/uaccess.h>
+#include <linux/path.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/dcache.h>
 
 struct pid_info {
 	int		pid;	
@@ -16,6 +23,30 @@ struct pid_info {
 
 SYSCALL_DEFINE2(get_pid_info, struct pid_info __user *, info, int, pid)
 {
-	printk(KERN_INFO "asking about info on : %d", pid);
-	return 0; 
+	ssize_t			retval = 0;
+	ssize_t			r = 0;
+	struct pid		*p;
+	struct task_struct	*task;
+	struct pid_info		tmp;
+/*	char			fullpath[512]; */
+
+	p = find_get_pid(pid);
+	task = pid_task(p, PIDTYPE_PID);
+	if (task) {
+		tmp.pid = task->pid;
+		tmp.parent = task->parent->pid;
+		tmp.stack = task->stack;
+		tmp.state = task->state;
+		tmp.start_time = task->start_time;
+	}
+	if ((r = copy_to_user(&tmp, info, sizeof(struct pid_info)))) {
+		retval = -EFAULT;
+		printk(KERN_INFO "copy to user fail with %ld\n", r);
+		goto out;
+	}
+
+	printk(KERN_INFO "calling sys_get_pid_info");
+	return retval;
+out:
+	return retval;
 }
